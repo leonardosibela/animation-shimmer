dae4bb7bf7e469e48468bebf7f922eca42aeb927
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-31 10:39:50 -0300

    Implementing main activity

--- app/src/main/java/com/sibela/facebookshimmer/MainActivity.kt
+++ app/src/main/java/com/sibela/facebookshimmer/MainActivity.kt
@@ -2,6 +2,9 @@ package com.sibela.facebookshimmer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sibela.facebookshimmer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupRecyclerView()
+        setupShimmerAnimationListener()
+    }
+
+    private fun setupRecyclerView() {
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        binding.programingSkillRecycler.apply {
+            addItemDecoration(decoration)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ProgrammingSkillAdapter(MockProgrammingSkill.getAll())
+        }
+    }
+
+    private fun setupShimmerAnimationListener() {
+        binding.apply {
+            startStopAnimation.setOnClickListener {
+                if (itemsShimmer.isShimmerStarted) {
+                    itemsShimmer.isVisible = false
+                    programingSkillRecycler.isVisible = true
+                    startStopAnimation.text = getString(R.string.title_start_animation)
+                    itemsShimmer.stopShimmer()
+                } else {
+                    itemsShimmer.isVisible = true
+                    programingSkillRecycler.isVisible = false
+                    startStopAnimation.text = getString(R.string.title_stop_animation)
+                    itemsShimmer.startShimmer()
+                }
+            }
+        }
     }
 }
